144270676b5bd58b22a410ea2308f6dd64502b02
shivaniii24 <64369944+shivaniii24@users.noreply.github.com>
2021-06-06 01:23:41 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1> Science</h1>
+      <p>
+        Science is the pursuit and application of knowledge and understanding of
+        the natural and social world following a systematic methodology based on
+        evidence
+      </p>
+    </div>
+  );
+}
+
+export default Note;
